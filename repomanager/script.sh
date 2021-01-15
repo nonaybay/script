5551bc5e2a73be64578ac81178f1bb03e731b4a2
nonaybay <rafaelvenancio@protonmail.com>
2021-01-15 09:52:57 +0000

    Read README

--- repomanager/script.sh
+++ repomanager/script.sh
@@ -3,10 +3,10 @@
 
 if [ "$1" == "debian" ]; then
 	sudo rm -rfv /etc/apt/sources.list
-	sudo cp ./debian.list /etc/apt/sources.list
+	sudo cp ./repo/debian.list /etc/apt/sources.list
 else
 	sudo rm -rfv /etc/apt/sources.list
-	sudo cp ./kali.list /etc/apt/sources.list
+	sudo cp ./repo/kali.list /etc/apt/sources.list
 fi
 
 clear
